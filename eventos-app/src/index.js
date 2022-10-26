ad3a84e8e020f3d5f0c1f4ba9d49b7b05fb47371
deimosribeiro1 <isabellar378@gmail.com>
2022-10-26 09:20:51 -0300

    Front-bootstrap

--- eventos-app/src/index.js
+++ eventos-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from '../src/view/login';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
